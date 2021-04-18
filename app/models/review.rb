451a1d87ae518721e912569937752534f4a72203
drodol <david@randsons.eu>
2021-04-18 21:50:13 +0200

    Add styles and validations in Review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,10 @@
 class Review < ApplicationRecord
+
+validates :title, presence: true
+validates :body, length: { minimum: 10 }
+validates :score, presence: true
+validates :restaurant, presence: true
+validates :price, presence: true
+validates :cuisine, presence: true
+
 end
